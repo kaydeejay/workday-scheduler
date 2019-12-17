06add3afe209706dedd1951ae654e4cb8a6d8161
Kevin Devine-Jones <kevin.jones215@gmail.com>
2019-12-17 01:27:45 -0500

    textareas change color based on hour

--- script.js
+++ script.js
@@ -11,5 +11,47 @@
 */
 
 $(document).ready(function(){
+
+    var colorTimer;
+
     $("#currentDay").text(moment());
+
+    function setColors(){
+        var actualTime = moment().hour();
+        var inputHours = $(".time-block");
+        
+        for (var i = 0; i < inputHours.length; i++){
+            var thisHour = $(inputHours[i])
+            var hourIndex = i+9;
+            $(thisHour).attr("data-hour", hourIndex);
+            if (hourIndex > actualTime) {
+                $(thisHour).addClass("past");
+            }
+            else if (hourIndex === actualTime) {
+                $(thisHour).addClass("present");
+            }
+            else {
+                $(thisHour).addClass("future");
+            }
+        }
+    }
+
+    setColors();
+    colorTimer = setTimeout(setColors, 60000);
+
+    
+
+    /**
+     * To Dos:
+     *    Get hour
+     *    Determine which colors display based on hour
+     *        Loop through the time/input divs
+     *        assign attribute data-hour equal to hour in 24-hr time format
+     *        compare each data-hour to current hour
+     *        < = past // === = present // > = future 
+     *    Display the colors:
+     *        set attribute "class" to .past/.present/.future
+     *         
+     *         
+     */
 });
